34f3e1497449205409e545a27f82b627384c18f3
Joao D Lopes <joao.d.lopes91@gmail.com>
2020-09-09 00:31:25 +0100

    rename testbench files; add adpll Makefile segment; change Makefile to use adpll Makefile segment

---
+++ adpll.mk
@@ -0,0 +1,7 @@
+ADPLL_OPERATION = 2 # PD = 0, TEST = 1, RX = 2, TX = 3
+FREQ_CHANNEL = 2433.500 # Channel freq in MHz
+SIM_TIME = 100 # simulation time in us
+DCO_PN = 1 # dco phase noise flag
+INIT_TIME_RM = 25 # initial transient time to remove in us (display purposes)
+
+DEFINE+=$(defmacro)FREQ_CHANNEL=$(FREQ_CHANNEL) $(defmacro)SIM_TIME=$(SIM_TIME) $(defmacro)DCO_PN=$(DCO_PN) $(defmacro)ADPLL_OPERATION=$(ADPLL_OPERATION)
